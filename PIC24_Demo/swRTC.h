dfd86823970e48bbe78b5309b72ae5a5495812e2
dannyf00 <fdan00@live.com>
2018-02-09 07:22:00 -0500

    Add files via upload

--- PIC24_Demo/swRTC.h
+++ PIC24_Demo/swRTC.h
@@ -1,6 +1,14 @@
 #ifndef SWRTC_H
 #define SWRTC_H
 
+//software implementation of a RTC
+//typical flow:
+//1. time_set() to initialize the timer
+//2. time_update() to update and obtain current time / second count (32-bit)
+//ticks can come from millis()/micros(), SysTick(), or any constantly running timer
+//TICKS_PER_SEC is user specified - similar to CLOCKS_PER_SEC, and 
+//may need to be adjusted based on prescaler setting
+
 #include "gpio.h"
 #include <time.h>							//we use time.h routines
 
@@ -9,7 +17,8 @@
 
 //global variables
 
-//update time
+//update time - need to be fed with a constantly increasing ticks value
+//update time_now
 time_t time_update(uint32_t ticks);
 
 //get time
